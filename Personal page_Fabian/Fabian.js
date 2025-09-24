ec4ab09bc362f51565b47e4bd0b91df7fe7b022e
Fabian144 <fabianyousif24@gmail.com>
2025-09-24 10:57:32 +0200

    Added folder and files for personal page and the link to it on the team page

---
+++ Personal page_Fabian/Fabian.js
@@ -0,0 +1,28 @@
+var right = document.querySelector(".buttonright");
+var left = document.querySelector(".buttonleft");
+var bild = document.querySelector(".bild");
+var bild2 = document.querySelector(".bild2");
+
+right.addEventListener("click", buttonright);
+
+function buttonright() {
+  if (bild.classList.contains("inactive") == false) {
+    bild.classList.toggle("inactive");
+    bild2.classList.toggle("active");
+    left.classList.toggle("active");
+    right.classList.toggle("inactive");
+    click = 1;
+  }
+}
+
+left.addEventListener("click", buttonleft);
+
+function buttonleft() {
+  if (bild.classList.contains("inactive") == true) {
+    bild.classList.toggle("inactive");
+    bild2.classList.toggle("active");
+    left.classList.toggle("active");
+    right.classList.toggle("inactive");
+    click = 0;
+  }
+}
