38f0e50c37cc770de95873c32c1ced1ad066aa2c
Fabian144 <fabianyousif24@gmail.com>
2025-09-29 12:52:52 +0200

    Commit section styling changes and updated code

--- scripts/Fabian.js
+++ scripts/Fabian.js
@@ -1,13 +1,13 @@
 var right = document.querySelector(".buttonright");
 var left = document.querySelector(".buttonleft");
-var bild = document.querySelector(".bild");
+var commits = document.querySelector("#commits_fabian");
 var bild2 = document.querySelector(".bild2");
 
 right.addEventListener("click", buttonright);
 
 function buttonright() {
-  if (bild.classList.contains("inactive") == false) {
-    bild.classList.toggle("inactive");
+  if (commits.classList.contains("inactive") == false) {
+    commits.classList.toggle("inactive");
     bild2.classList.toggle("active");
     left.classList.toggle("active");
     right.classList.toggle("inactive");
@@ -17,8 +17,8 @@ function buttonright() {
 left.addEventListener("click", buttonleft);
 
 function buttonleft() {
-  if (bild.classList.contains("inactive") == true) {
-    bild.classList.toggle("inactive");
+  if (commits.classList.contains("inactive") == true) {
+    commits.classList.toggle("inactive");
     bild2.classList.toggle("active");
     left.classList.toggle("active");
     right.classList.toggle("inactive");
