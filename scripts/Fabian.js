833bd68a613c883a119da6a92c723542d7697c0d
Fabian144 <fabianyousif24@gmail.com>
2025-10-02 14:46:51 +0200

    Responsiveness fixes and image carousel done

--- scripts/Fabian.js
+++ scripts/Fabian.js
@@ -1,22 +1,29 @@
 var right = document.querySelector(".buttonright");
 var left = document.querySelector(".buttonleft");
 var commits = document.querySelector("#commits_fabian");
-var bild = document.querySelector(".bild");
+var bild1 = document.querySelector(".bild1");
+var bild2 = document.querySelector(".bild2");
 
 right.addEventListener("click", () => {
-  if (commits.classList.contains("inactive") == false) {
-    commits.classList.toggle("inactive");
-    bild.classList.toggle("active");
-    left.classList.toggle("active");
-    right.classList.toggle("inactive");
+  if (!commits.classList.contains("inactive")) {
+    commits.classList.add("inactive");
+    bild1.classList.add("active");
+    left.classList.add("active");
+  } else if (bild1.classList.contains("active")) {
+    bild1.classList.remove("active");
+    bild2.classList.add("active");
+    right.classList.add("inactive");
   }
 });
 
 left.addEventListener("click", () => {
-  if (commits.classList.contains("inactive") == true) {
-    commits.classList.toggle("inactive");
-    bild.classList.toggle("active");
-    left.classList.toggle("active");
-    right.classList.toggle("inactive");
+  if (bild2.classList.contains("active")) {
+    bild1.classList.add("active");
+    bild2.classList.remove("active");
+    right.classList.remove("inactive");
+  } else if (bild1.classList.contains("active")) {
+    commits.classList.remove("inactive");
+    bild1.classList.remove("active");
+    left.classList.remove("active");
   }
 });
