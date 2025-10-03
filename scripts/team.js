5c2a0af5ca6d0969c02278a0d0e78d61e38932ca
Fabian144 <fabianyousif24@gmail.com>
2025-10-03 12:44:18 +0200

    Added animation for the team page

---
+++ scripts/team.js
@@ -0,0 +1,11 @@
+var siri = document.querySelector(".profilecomp.siri");
+var axel = document.querySelector(".profilecomp.axel");
+var fabian = document.querySelector(".profilecomp.fabian");
+
+function profileOpacity() {
+  siri.style.opacity = "1";
+	axel.style.opacity = "1";
+	fabian.style.opacity = "1";
+}
+
+setTimeout(profileOpacity, 1000);
