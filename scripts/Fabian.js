17d7fcbdc5cab4874d5ddfcdca1ab2ed9f30017e
Fabian144 <fabianyousif24@gmail.com>
2025-10-08 14:52:06 +0200

    Fixed script after class name changes

--- scripts/Fabian.js
+++ scripts/Fabian.js
@@ -1,47 +1,47 @@
 var right = document.querySelector(".buttonright");
 var left = document.querySelector(".buttonleft");
 var commits = document.querySelector("#commits_fabian");
-var bild1 = document.querySelector(".bild1");
-var bild2 = document.querySelector(".bild2");
-var bild3 = document.querySelector(".bild3");
+var img1 = document.querySelector(".img1");
+var img2 = document.querySelector(".img2");
+var img3 = document.querySelector(".img3");
 
 right.addEventListener("click", () => {
   if (!commits.classList.contains("inactive")) {
     setTimeout(() => {
       commits.classList.add("inactive");
-      bild1.classList.add("active");
+      img1.classList.add("active");
       left.classList.add("active");
     }, 100);
-  } else if (bild1.classList.contains("active")) {
+  } else if (img1.classList.contains("active")) {
     setTimeout(() => {
-      bild1.classList.remove("active");
-      bild2.classList.add("active");
+      img1.classList.remove("active");
+      img2.classList.add("active");
     }, 100);
-  } else if (bild2.classList.contains("active")) {
+  } else if (img2.classList.contains("active")) {
     setTimeout(() => {
-      bild2.classList.remove("active");
-      bild3.classList.add("active");
+      img2.classList.remove("active");
+      img3.classList.add("active");
       right.classList.add("inactive");
     }, 100);
   }
 });
 
 left.addEventListener("click", () => {
-  if (bild3.classList.contains("active")) {
+  if (img3.classList.contains("active")) {
     setTimeout(() => {
-      bild2.classList.add("active");
-      bild3.classList.remove("active");
+      img2.classList.add("active");
+      img3.classList.remove("active");
       right.classList.remove("inactive");
     }, 100);
-  } else if (bild2.classList.contains("active")) {
+  } else if (img2.classList.contains("active")) {
     setTimeout(() => {
-      bild1.classList.add("active");
-      bild2.classList.remove("active");
+      img1.classList.add("active");
+      img2.classList.remove("active");
     }, 100);
-  } else if (bild1.classList.contains("active")) {
+  } else if (img1.classList.contains("active")) {
     setTimeout(() => {
       commits.classList.remove("inactive");
-      bild1.classList.remove("active");
+      img1.classList.remove("active");
       left.classList.remove("active");
     }, 100);
   }
