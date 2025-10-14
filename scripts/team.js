3a4987b8e439683e3b7ddc20670a85cd0e759eeb
Fabian144 <fabianyousif24@gmail.com>
2025-10-14 10:22:18 +0200

    Profile comp info added through json string

--- scripts/team.js
+++ scripts/team.js
@@ -25,3 +25,34 @@ function mobileAnimation() {
     window.removeEventListener("scroll", mobileAnimation);
   }
 }
+
+const membersJsonString = `{
+	"members": [
+		{
+			"name": "Siri",
+			"description": "Grafisk designer som vill bredda sig inom webb. Gillar alla möjliga kreativa grejer och hoppas att programmering kan bli mitt nästa kreativa utlopp."
+		},
+		{
+			"name": "Axel",
+			"description": "Älskar att lära mig nya saker, brinner för utforskande, skapande, förbättring och framförallt att ha kul under tiden. Letar alltid efter nya projekt och äventyr."
+		},
+		{
+			"name": "Fabian",
+			"description": "Nyfiken och lösningsorienterad. Har byggt en del av sidan som du är på just nu och förhoppningsvis många fler i framtiden :)"
+		}
+	]
+}`;
+
+function parseJson(membersAsString) {
+  return JSON.parse(membersAsString);
+}
+
+const membersData = parseJson(membersJsonString);
+
+document.querySelector(".profilelink.siri").innerText = membersData.members[0].name;
+document.querySelector(".profilelink.axel").innerText = membersData.members[1].name;
+document.querySelector(".profilelink.fabian").innerText = membersData.members[2].name;
+
+document.querySelector(".profiletext.siri").innerText = membersData.members[0].description;
+document.querySelector(".profiletext.axel").innerText = membersData.members[1].description;
+document.querySelector(".profiletext.fabian").innerText = membersData.members[2].description;
